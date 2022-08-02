c28ddeffe70a6ea20b0567b64dc0eb329bcc503a
Valerii Hanchak <hanchakvalerii@gmail.com>
2022-08-02 19:17:24 +0000

    fixed

--- Lesson7/status1.sh
+++ Lesson7/status1.sh
@@ -6,7 +6,7 @@ if [[ -n "$pids" ]]; then
         arr=($pids)
         name=$(ps -p ${arr[0]} -o comm=)
         echo "Process $name - ${arr[0]} stopped"
-        sudo kill 17 "${arr[0]}"
+        sudo -A kill 17 "${arr[0]}"
 fi
 sudo service apache2 start
 
@@ -16,5 +16,5 @@ if [[ -n "$pids" ]]; then
         arr=($pids)
         name=$(ps -p ${arr[0]} -o comm=)
         echo "Process $name - ${arr[0]} terminated"
-        sudo kill 9 "${arr[0]}"
+        sudo -A kill 9 "${arr[0]}"
 fi
