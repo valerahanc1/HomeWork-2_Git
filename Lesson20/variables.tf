69906e1484323b4fb3fa3e79c110b7d290466124
Ubuntu <ubuntu@ip-172-31-33-49.eu-central-1.compute.internal>
2022-09-12 18:07:20 +0000

    Lesson20

---
+++ Lesson20/variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group_location" {
+  default     = "westeurope"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  default     = "azure-terraform-test"
+  description = "Name of the resource group."
+}
